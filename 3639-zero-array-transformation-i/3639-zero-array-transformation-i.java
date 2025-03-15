3ddad7c6f863a7033cd6927837d2e601c052f4b6
vinod kumar peddi <119839782+vinodkumarpeddi@users.noreply.github.com>
2025-03-15 19:18:30 +0530

    Time: 3 ms (79.82%), Space: 94.9 MB (63.13%) - LeetHub

--- 3639-zero-array-transformation-i/3639-zero-array-transformation-i.java
+++ 3639-zero-array-transformation-i/3639-zero-array-transformation-i.java
@@ -1,24 +1,20 @@
 class Solution {
     public boolean isZeroArray(int[] nums, int[][] queries) {
-        int n=nums.length;
+        int n=nums.length,currsum=0;
         int line[]=new int[n+1];
-        Arrays.fill(line,0);
+
+     
         for(int query[]:queries)
         {
-            line[query[0]]+=1;
-            line[query[1]+1]-=1;
+            line[query[0]]++;
+            line[query[1]+1]--;
         }
-        for(int i=1;i<line.length;i++)
+        for(int i=0;i<line.length-1;i++)
         {
-           line[i]=line[i]+line[i-1];
+           currsum+=line[i];
+           if(nums[i]>currsum) return false;
         }
-        for(int i=0;i<nums.length;i++)
-        {
-            if(line[i]<nums[i])
-            {
-                return false;
-            }
-        }
+       
         return true;
     }
 }
