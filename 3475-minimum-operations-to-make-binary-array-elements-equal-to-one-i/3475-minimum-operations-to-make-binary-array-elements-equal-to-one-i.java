93467bd6ee22a70bedcb7ce96974adda23932067
vinod kumar peddi <119839782+vinodkumarpeddi@users.noreply.github.com>
2025-03-19 10:00:44 +0530

    Time: 6 ms (100%), Space: 58 MB (9.04%) - LeetHub

--- 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i.java
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i.java
@@ -7,8 +7,8 @@ class Solution {
             if(nums[i]==0)
             {
                 nums[i]=1;
-                nums[i+1]^=1;
-                nums[i+2]^=1;
+                nums[i+1]=nums[i+1]==0?1:0;
+                nums[i+2]=nums[i+2]==0?1:0;
                 c++;
             }
         }
