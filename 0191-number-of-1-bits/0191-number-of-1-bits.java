40eb5ec28f04a2dc7868ef1551ab9fbd6bb89383
vinod kumar peddi <119839782+vinodkumarpeddi@users.noreply.github.com>
2025-03-19 23:32:33 +0530

    Time: 1 ms (17.43%), Space: 41.3 MB (7.97%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.java
+++ 0191-number-of-1-bits/0191-number-of-1-bits.java
@@ -3,8 +3,8 @@ class Solution {
         int cnt=0;
         while(n!=0)
         {
-            cnt+=n&1;
-            n>>=1;
+           n=n&n-1;
+           cnt+=1;
         }
         return cnt;
 
