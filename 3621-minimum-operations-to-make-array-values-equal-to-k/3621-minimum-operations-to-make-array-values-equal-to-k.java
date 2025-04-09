7700a1ffa7ec26a5de9c47891c9fc170201596fd
vinod kumar peddi <119839782+vinodkumarpeddi@users.noreply.github.com>
2025-04-09 18:48:45 +0530

    Time: 2 ms (82.71%), Space: 44.4 MB (55.59%) - LeetHub

---
+++ 3621-minimum-operations-to-make-array-values-equal-to-k/3621-minimum-operations-to-make-array-values-equal-to-k.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minOperations(int[] nums, int k) {
+        HashSet<Integer> set=new HashSet<>();
+        for(int num:nums)
+        {
+            if(num<k)
+            {
+                return -1;
+            }
+            else if(num>k)
+            {
+                set.add(num);
+            }
+        }
+        return set.size();
+    }
+}
