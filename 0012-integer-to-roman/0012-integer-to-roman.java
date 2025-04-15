2a33359b250b1eefc0fffa94e8557e941ba3ceae
vinod kumar peddi <119839782+vinodkumarpeddi@users.noreply.github.com>
2025-04-15 20:05:33 +0530

    Time: 3 ms (98.35%), Space: 44.7 MB (40.33%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -1,19 +1,17 @@
 class Solution {
     public String intToRoman(int num) {
-        String roman = "";
+        StringBuilder roman = new StringBuilder();
         String[] romanchar = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
         int[] nums = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
 
-        for (int pos = 0; num > 0; pos++) {
-             int x=num/nums[pos];
-             while(x>0)
-             {
-                roman+=romanchar[pos];
-                x--;
-             }
-             num=num%nums[pos];
+        for (int i = 0; i < nums.length && num > 0; i++) {
+            int count = num / nums[i];
+            num %= nums[i];
+            while (count-- > 0) {
+                roman.append(romanchar[i]);
+            }
         }
 
-        return roman;
+        return roman.toString();
     }
 }
