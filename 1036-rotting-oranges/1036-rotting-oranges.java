361f562d72c8ca550be5a46fe02212927a07223f
vinod kumar peddi <119839782+vinodkumarpeddi@users.noreply.github.com>
2025-08-11 02:49:25 -1100

    Time: 1 ms (100%), Space: 43 MB (10.96%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.java
+++ 1036-rotting-oranges/1036-rotting-oranges.java
@@ -3,62 +3,55 @@ class Solution {
         int row;
         int col;
         int time;
-        Pair(int row,int col,int time)
+        public Pair(int row,int col,int time)
         {
             this.row=row;
             this.col=col;
             this.time=time;
         }
+
     }
     public int orangesRotting(int[][] grid) {
-        Queue<Pair> q=new LinkedList<>();
-       
-           int n=grid.length;
-           int m=grid[0].length;
-           int fresh=0,rotten=0,time=0;
-            boolean vis[][]=new boolean[n][m];
-           int delrow[]={-1,0,1,0};
-           int delcol[]={0,1,0,-1};
-           for(int i=0;i<n;i++)
-           {
-            for(int j=0;j<m;j++)
-            {
-                if(grid[i][j]==2)
-                {
-                     q.add(new Pair(i,j,0));
-                     vis[i][j]=true;
-                }
-                else if(grid[i][j]==1)
-                {
-                    fresh++;
-                }
+       int n=grid.length;
+        int m=grid[0].length;
+         Queue<Pair> q=new LinkedList<>();
+          int freshCount = 0;
 
+        // Step 1: Push all initially rotten oranges into the queue
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 2) {
+                    q.add(new Pair(i, j, 0));
+                }
+                if (grid[i][j] == 1) {
+                    freshCount++;
+                }
             }
-           }
-            while(!q.isEmpty())
+        }
+         int delrow[]={-1,0,1,0};
+         int delcol[]={0,1,0,-1};
+         int time=0;
+         int rotten=0;
+         
+         while(!q.isEmpty())
          {
-            Pair current=q.poll();
-            int row=current.row;
-            int col=current.col;
-            int t=current.time;
+             Pair node=q.poll();
+            int r=node.row;
+            int c=node.col;
+            int t=node.time;
             time=Math.max(time,t);
+            for(int k=0;k<4;k++)
+            {
+                int nrow=r+delrow[k];
+                int ncol=c+delcol[k];
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]==1)
+                {
+                    grid[nrow][ncol]=2;
+                    q.add(new Pair(nrow,ncol,t+1));
+                    rotten++;
+                }
+            }
             
-              for(int i=0;i<4;i++)
-              {
-                   int nrow=row+delrow[i];
-                   int ncol=col+delcol[i];
-                   if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]==1 && !vis[nrow][ncol])
-                   {
-                       
-                       q.add(new Pair(nrow,ncol,time+1));
-                       vis[nrow][ncol]=true;
-                       rotten++;
-                   }
-              }
-             
          }
-         return (rotten==fresh)?time:-1;
-          
-          
-    }
-}
+         return (freshCount-rotten)!=0?-1:time;
+}}
