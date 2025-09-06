7d9a484d1a0f8fbaaf85eea61fef7de5899e6532
vinod kumar peddi <119839782+vinodkumarpeddi@users.noreply.github.com>
2025-09-06 10:53:46 +0530

    Time: 366 ms (65.59%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,3 +1,5 @@
-# Write your MySQL query statement below
-select MAX(salary) AS SecondHighestSalary from Employee
-where salary<(select MAX(salary) from Employee)
+/* Write your PL/SQL query statement below */
+select max(salary) as SecondHighestSalary
+from Employee
+where
+salary<(select max(salary) from Employee);
